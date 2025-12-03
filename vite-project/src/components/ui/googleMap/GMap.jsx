817c61d9c7e11d0ea7a8dc4729637b2cf1f69465
Oleh.P <oleg.poruchikov@gmail.com>
2025-12-03 16:55:05 +0200

    Enhance GMap component by replacing Marker with CustomMarker for improved styling and interactivity. Add LocationOnIcon and custom styles for marker display. Update main CSS to style Google Map elements for better visibility.

--- vite-project/src/components/ui/googleMap/GMap.jsx
+++ vite-project/src/components/ui/googleMap/GMap.jsx
@@ -1,9 +1,15 @@
 import { Suspense } from "react";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import {
 	GoogleMap,
 	GoogleMapApiLoader,
 	Marker,
+	CustomMarker,
+	AdvancedMarker,
+	PinElement,
 } from "react-google-map-wrapper";
+
+import styles from "./GMap.module.scss";
 
 function GMap() {
 	const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -19,7 +25,16 @@ function GMap() {
 						colorScheme: "DARK",
 					}}
 					center={{ lat: 49.99376313381941, lng: 36.23106023674629 }}>
-					<Marker lat={49.99376313381941} lng={36.23106023674629} />
+					<CustomMarker
+						lat={49.99376313381941}
+						lng={36.23106023674629}>
+						<div className={styles.pointer}>
+							<span className={styles.icon}>
+								<LocationOnIcon />
+							</span>
+							<span className={styles.title}>Sport Time</span>
+						</div>
+					</CustomMarker>
 				</GoogleMap>
 			</GoogleMapApiLoader>
 		</Suspense>
