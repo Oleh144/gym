0b920a53f722492cad4405285a82ccb125b1350f
Oleh.P <oleg.poruchikov@gmail.com>
2025-12-03 17:24:00 +0200

    Add Polyline to GMap component to visualize path to gym with custom styling. Enhance map interactivity and user experience.

--- vite-project/src/components/ui/googleMap/GMap.jsx
+++ vite-project/src/components/ui/googleMap/GMap.jsx
@@ -7,12 +7,21 @@ import {
 	CustomMarker,
 	AdvancedMarker,
 	PinElement,
+	Polyline,
 } from "react-google-map-wrapper";
 
 import styles from "./GMap.module.scss";
 
 function GMap() {
 	const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+	const pathToGym = [
+		{ lat: 49.99336669405517, lng: 36.23162977384829 },
+		{ lat: 49.993442564451705, lng: 36.23110942535665 },
+		{ lat: 49.99363223991932, lng: 36.2311711161572 },
+		{ lat: 49.99363569616529, lng: 36.231105428405726 },
+		{ lat: 49.993667181925126, lng: 36.23110566266259 },
+	];
 
 	return (
 		<Suspense>
@@ -25,6 +34,13 @@ function GMap() {
 						colorScheme: "DARK",
 					}}
 					center={{ lat: 49.99376313381941, lng: 36.23106023674629 }}>
+					<Polyline
+						path={pathToGym}
+						strokeColor="#FF0000"
+						strokeOpacity={1.0}
+						strokeWeight={2}
+						geodesic
+					/>
 					<CustomMarker
 						lat={49.99376313381941}
 						lng={36.23106023674629}>
